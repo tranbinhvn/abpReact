eec48563da11402e769e84caae1becec1f4f866e
Tran Binh <binh.tran@ncc.asia>
2019-11-15 20:48:23 +0700

    add book-author-category entity

---
+++ aspnet-core/src/abpReact.Web.Core/Models/AuthorManager.cs
@@ -0,0 +1,58 @@
+﻿using Abp.Domain.Repositories;
+using Abp.Domain.Services;
+using Abp.UI;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace reactjs.Models
+{
+    public class AuthorManager : DomainService, IAuthorManager
+    {
+        private readonly IRepository<Author> _repoAuthor;
+        public AuthorManager(IRepository<Author> repoAuthor)
+        {
+            _repoAuthor = repoAuthor;
+        }
+        public async Task<Author> Create(Author entity)
+        {
+            var author = _repoAuthor.FirstOrDefault(x => x.Id == entity.Id);
+            if (author != null)
+            {
+                throw new UserFriendlyException("Already Exist");
+            }
+            else
+            {
+                return await _repoAuthor.InsertAsync(entity);
+            }
+
+        }
+
+        public void Delete(int id)
+        {
+            var author = _repoAuthor.FirstOrDefault(x => x.Id == id);
+            if(author == null)
+            {
+                throw new UserFriendlyException("No Data Found");
+            }
+            else
+            {
+                _repoAuthor.Delete(author);
+            }
+        }
+
+        public IEnumerable<Author> GetAllList()
+        {
+            return _repoAuthor.GetAllIncluding(x => x.Books);
+        }
+
+        public Author GetAuthorById(int id)
+        {
+            return _repoAuthor.Get(id);
+        }
+
+        public void Update(Author entity)
+        {
+            _repoAuthor.Update(entity);
+        }
+    }
+}
---
+++ aspnet-core/src/abpReact.Web.Core/Models/CategoryManager.cs
@@ -0,0 +1,58 @@
+﻿using Abp.Domain.Repositories;
+using Abp.Domain.Services;
+using Abp.UI;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace reactjs.Models
+{
+    public class CategoryManager : DomainService, ICategoryManager
+    {
+        private readonly IRepository<Category> _repoCategory;
+        public CategoryManager(IRepository<Category> repoCategory)
+        {
+            _repoCategory = repoCategory;
+        }
+        public async Task<Category> Create(Category entity)
+        {
+            var Category = _repoCategory.FirstOrDefault(x => x.Id == entity.Id);
+            if (Category != null)
+            {
+                throw new UserFriendlyException("Already Exist");
+            }
+            else
+            {
+                return await _repoCategory.InsertAsync(entity);
+            }
+
+        }
+
+        public void Delete(int id)
+        {
+            var Category = _repoCategory.FirstOrDefault(x => x.Id == id);
+            if (Category == null)
+            {
+                throw new UserFriendlyException("No Data Found");
+            }
+            else
+            {
+                _repoCategory.Delete(Category);
+            }
+        }
+
+        public IEnumerable<Category> GetAllList()
+        {
+            return _repoCategory.GetAllIncluding(x => x.Books);
+        }
+
+        public Category GetCategoryById(int id)
+        {
+            return _repoCategory.Get(id);
+        }
+
+        public void Update(Category entity)
+        {
+            _repoCategory.Update(entity);
+        }
+    }
+}
